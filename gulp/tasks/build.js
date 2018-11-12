f9988f6031a0592cfbb97f53733f9ef57b52a344
Alexander Bird <alexkbird@gmail.com>
2018-11-12 01:18:29 -0700

    Move from json content to yaml content

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -3,11 +3,13 @@ const gulp = require('gulp');
 const gulpJsonHandlebars = require('gulp-json-handlebars');
 const path = require('path');
 const removeJsonFileNameSuffix = () => require('gulp-ext-replace')('');
+const yaml = require('gulp-yaml');
 
 const { buildDirectory, sourceDirectory, handlebars: handlebarsOptions } = require('../config')
 
 gulp.task('build', () => gulp
-  .src(`${sourceDirectory}/content/**/*.html.json`)
+  .src(`${sourceDirectory}/content/**/*.html.yaml`)
+  .pipe(yaml())
   .pipe(gulpJsonHandlebars(handlebarsOptions, getPageTemplate))
   .pipe(removeJsonFileNameSuffix())
   .pipe(gulp.dest(buildDirectory))
