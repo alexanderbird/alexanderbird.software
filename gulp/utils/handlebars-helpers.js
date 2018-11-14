a71f4267e1687f2bd8f884acad94d2c02b9f7319
Alexander Bird <alexkbird@gmail.com>
2018-11-14 11:06:40 -0700

    Add links to main page

--- gulp/utils/handlebars-helpers.js
+++ gulp/utils/handlebars-helpers.js
@@ -1,3 +1,5 @@
 const handlebars = require('handlebars');
+require('handlebars-inline').register(handlebars)
 
 handlebars.registerHelper('coalesce', (...args) => args.find(i => !!i));
+handlebars.registerHelper('concat', (...args) => args.pop() && args.join(''))
