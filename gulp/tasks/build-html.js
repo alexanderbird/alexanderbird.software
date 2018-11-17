442bd2df41ee469020f26fc4b3efcf861df1c1f5
Alexander Bird <alexkbird@gmail.com>
2018-11-17 00:01:49 -0700

    Support multiple pages in their own top-level div

--- gulp/tasks/build-html.js
+++ gulp/tasks/build-html.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const gulp = require('gulp');
 const gulpJsonHandlebars = require('gulp-json-handlebars');
+const gulpHtmlHandlebars = require('gulp-handlebars-html')();
+const concat = require('gulp-concat');
+const wrap = require('gulp-wrap');
+const extensionReplace = require('gulp-ext-replace');
 const path = require('path');
-const removeJsonFileNameSuffix = () => require('gulp-ext-replace')('');
 const yaml = require('gulp-yaml');
 
 require('../utils/handlebars-helpers')
@@ -13,7 +16,9 @@ gulp.task('build:html', () => gulp
   .src(`${sourceDirectory}/content/**/*.html.yaml`)
   .pipe(yaml())
   .pipe(gulpJsonHandlebars(handlebarsOptions, getPageTemplate))
-  .pipe(removeJsonFileNameSuffix())
+  .pipe(concat('index.html'))
+  .pipe(wrap({ src: path.join(sourceDirectory, 'index.html.hbs') }))
+  .pipe(gulpHtmlHandlebars({}, { partialsDirectory: [handlebarsOptions.partialsDirectory] }))
   .pipe(gulp.dest(buildDirectory))
 );
 
