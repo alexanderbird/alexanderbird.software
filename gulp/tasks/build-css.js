ff9133b2bae01b630201cafd98ac9bdbace27e94
Alexander Bird <alexkbird@gmail.com>
2018-12-27 11:52:32 -0700

    Fix browser compatibility issue by replacing initialization animation with initialization JavaScript DOMContentReady event

--- gulp/tasks/build-css.js
+++ gulp/tasks/build-css.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
 
 const {sourceDirectory, buildDirectory} = require('../config');
 
@@ -9,5 +10,9 @@ gulp.task('build:css', () =>
     .src(`${sourceDirectory}/**/*.scss`)
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(concat('site.css'))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest(buildDirectory)),
 );
