fd00b8b11bb4cdcb87c9f6476e6184217115f595
Alexander Bird <alexkbird@gmail.com>
2019-01-19 07:30:44 -0700

    Add signature.js for signing the console

--- gulp/tasks/build-assets.js
+++ gulp/tasks/build-assets.js
@@ -1,7 +1,12 @@
 const gulp = require('gulp');
-const {sourceAssetsDirectory, buildDirectory} = require('../config');
+const {sourceAssetsDirectory, sourceRawPagesDirectory, buildDirectory} = require('../config');
 
-gulp.task('build:assets', () => gulp.src(`${sourceAssetsDirectory}/**/*`)
+gulp.task('build:actual-assets', () => gulp.src(`${sourceAssetsDirectory}/**/*`)
     .pipe(gulp.dest(`${buildDirectory}/assets`))
 );
 
+gulp.task('build:raw-pages', () => gulp.src(`${sourceRawPagesDirectory}/*`)
+    .pipe(gulp.dest(buildDirectory))
+);
+
+gulp.task('build:assets', gulp.parallel('build:actual-assets', 'build:raw-pages'))
