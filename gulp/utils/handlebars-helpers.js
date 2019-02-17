d12ba902dfc89fcae8ef534bb903220bc96455af
Alexander Bird <alex@alexanderbird.software>
2019-02-16 17:17:27 -0700

    Update skill sections text; fixed the spacing on the skills section

--- gulp/utils/handlebars-helpers.js
+++ gulp/utils/handlebars-helpers.js
@@ -6,3 +6,4 @@ require('handlebars-inline').register(handlebars)
 handlebars.registerHelper('markdown', markdown => marked(markdown));
 handlebars.registerHelper('coalesce', (...args) => args.find(i => !!i));
 handlebars.registerHelper('concat', (...args) => args.pop() && args.join(''))
+handlebars.registerHelper('is-array', thing => Array.isArray(thing))
