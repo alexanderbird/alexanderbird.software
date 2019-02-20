efc748f4d3d982f1d025257b845a7b4adffb7d48
Alexander Bird <alex@alexanderbird.software>
2019-02-20 09:43:16 -0700

    Draft: add experience sections

--- gulp/utils/handlebars-helpers.js
+++ gulp/utils/handlebars-helpers.js
@@ -7,3 +7,18 @@ handlebars.registerHelper('markdown', markdown => marked(markdown));
 handlebars.registerHelper('coalesce', (...args) => args.find(i => !!i));
 handlebars.registerHelper('concat', (...args) => args.pop() && args.join(''))
 handlebars.registerHelper('is-array', thing => Array.isArray(thing))
+handlebars.registerHelper('repeat', (count, item) => Array(count + 1).join(item))
+handlebars.registerHelper('json', data => JSON.stringify(data, null, 2))
+handlebars.registerHelper('experience', data => new Experience(data))
+
+function Experience(data) {
+  if(typeof data === 'string') {
+    this.isString = true
+    this.value = data
+  } else {
+    const keys = Object.keys(data)
+    if(keys.length != 1) throw new Error(`ArgumentError: data must have exactly one key. Got ${JSON.stringify(data, null, 2)}`)
+    this.type = keys[0]
+    this.value = data[this.type]
+  }
+}
