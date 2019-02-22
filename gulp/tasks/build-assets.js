dbc3dd4e21be3dfc9320d75f9486dc7af1359727
Alexander Bird <alex@alexanderbird.software>
2019-02-21 20:59:03 -0700

    WIP: introduce svg symbol library; update test files to prepare for regression testing

--- gulp/tasks/build-assets.js
+++ gulp/tasks/build-assets.js
@@ -1,12 +1,31 @@
-const gulp = require('gulp');
-const {sourceAssetsDirectory, sourceRawPagesDirectory, buildDirectory} = require('../config');
+const gulp = require('gulp')
+const svgSprite = require('gulp-svg-sprite')
+const rename = require('gulp-rename')
+const path = require('path')
+const {
+  sourceAssetsDirectory,
+  sourceIconsDirectory,
+  sourceRawPagesDirectory,
+  buildDirectory,
+  buildAssetsDirectory,
+  buildIconsBasename
+} = require('../config')
 
 gulp.task('build:actual-assets', () => gulp.src(`${sourceAssetsDirectory}/**/*`)
-    .pipe(gulp.dest(`${buildDirectory}/assets`))
+    .pipe(gulp.dest(path.join(buildDirectory, buildAssetsDirectory)))
+);
+
+gulp.task('build:svg-icons', () => gulp.src(`${sourceIconsDirectory}/**/*.svg`)
+    .pipe(svgSprite({ mode: { symbol: true } }))
+    .pipe(rename({
+      dirname: buildAssetsDirectory,
+      basename: buildIconsBasename
+    }))
+    .pipe(gulp.dest(buildDirectory))
 );
 
 gulp.task('build:raw-pages', () => gulp.src(`${sourceRawPagesDirectory}/*`)
     .pipe(gulp.dest(buildDirectory))
 );
 
-gulp.task('build:assets', gulp.parallel('build:actual-assets', 'build:raw-pages'))
+gulp.task('build:assets', gulp.parallel('build:actual-assets', 'build:svg-icons', 'build:raw-pages'))
