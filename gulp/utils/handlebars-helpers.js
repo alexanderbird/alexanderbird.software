8348ff066615c72cfafb04be691173e0eb1f4afb
Alexander Bird <alex@alexanderbird.software>
2019-03-03 22:05:12 -0700

    Introduce prettier tech stack diagrams -- after simplifying the yaml syntax, ready to migrate all the tech stacks; also added media query for prefers reduced motion

--- gulp/utils/handlebars-helpers.js
+++ gulp/utils/handlebars-helpers.js
@@ -4,8 +4,9 @@ const pluralize = require('pluralize');
 
 require('handlebars-inline').register(handlebars)
 
-handlebars.registerHelper('markdown', markdown => marked(markdown));
-handlebars.registerHelper('coalesce', (...args) => args.find(i => !!i));
+handlebars.registerHelper('not', object => !object)
+handlebars.registerHelper('markdown', markdown => marked(markdown))
+handlebars.registerHelper('coalesce', (...args) => args.find(i => !!i))
 handlebars.registerHelper('concat', (...args) => args.pop() && args.join(''))
 handlebars.registerHelper('is-array', thing => Array.isArray(thing))
 handlebars.registerHelper('repeat', (count, item) => Array(count + 1).join(item))
@@ -13,14 +14,24 @@ handlebars.registerHelper('json', data => JSON.stringify(data, null, 2))
 handlebars.registerHelper('experience', data => new Experience(data))
 handlebars.registerHelper('pluralize', pluralize)
 
-function Experience(data) {
-  if(typeof data === 'string') {
-    this.isString = true
-    this.value = data
-  } else {
-    const keys = Object.keys(data)
-    if(keys.length != 1) throw new Error(`ArgumentError: data must have exactly one key. Got ${JSON.stringify(data, null, 2)}`)
-    this.type = keys[0]
-    this.value = data[this.type]
+class Experience {
+  constructor(data) {
+    if(typeof data === 'string') {
+      this.isString = true
+      this.value = data
+    } else {
+      const keys = Object.keys(data)
+      if(keys.length != 1) throw new Error(`ArgumentError: data must have exactly one key. Got ${JSON.stringify(data, null, 2)}`)
+      this.type = keys[0]
+      this.value = data[this.type]
+    }
+  }
+
+  get isGroup() {
+    return this.type === 'group'
+  }
+
+  get isConnection() {
+    return this.type === 'connection'
   }
 }
